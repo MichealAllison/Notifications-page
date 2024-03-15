e34a56b9af9b436c923463df9f41c7a6184f78c4
MichealAllison <michealallison220@gmail.com>
2024-03-15 05:31:01 +0100

    I added some DOM Manupulation

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".mark-all").addEventListener("click", function () {
-    document.querySelector(".notifications").style.backgroundColor =
+    document.querySelector(".notification-count").style.display = "none";
+
+    document.querySelector(".notification").style.backgroundColor =
       "hsl(0, 0%, 100%)";
+
+    document.querySelector(".notification-dot").style.display = "none";
   });
+
+  //   documentElement
+  //     .querySelector(".notification")
+  //     .addEventListener("click", function () {
+  //       document.querySelector(".notification-dot").style.display = "none";
+  //       document.querySelector(".notification").style.backgroundColor =
+  //         "hsl(0, 0%, 100%)";
+  //     });
 });
