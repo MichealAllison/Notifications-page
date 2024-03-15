bad74ce003ae2f8a0f028601e4b139df57aabd3e
MichealAllison <michealallison220@gmail.com>
2024-03-15 05:29:55 +0100

    i added DOM Manipulation

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".mark-all").addEventListener("click", function () {
+    document.querySelector(".notifications").style.backgroundColor =
+      "hsl(0, 0%, 100%)";
+  });
+});
